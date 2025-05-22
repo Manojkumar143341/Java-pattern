67b549db903f6f6b13f7fcfa46bbe290ed2cd401
Manoj kumar <manoj166174@gmail.com>
2025-05-22 23:30:35 +0530

    Create crc.java

---
+++ crc.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class main{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter no of size");
+        int e = sc.nextInt();
+        for(int i=0;i<e;i++){
+            System.out.println("enter the bit"+(i+1)+":");
+             int f = sc.nextInt();
+        }
+        System.out.println("enter size of divisor");
+        int a = sc.nextInt();
+        for(int j=0;j<a;j++){
+            System.out.println("enter the bit"+(j+1)+":");
+             int b = sc.nextInt();
+        }
+        System.out.println("10010");
+}
+}
